6a9ba1a941d94dd8d65ad9209effe8bae71ed746
Wagner de Almeida <wagneer.alm@gmail.com>
2024-01-12 17:49:42 -0300

    User entity and resource

---
+++ course/src/main/java/com/wagneralm/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.wagneralm.course.resources;
+
+import com.wagneralm.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+
+        User u = new User(1l, "Maria", "maria@email.com", "99999999", "12345");
+
+        return ResponseEntity.ok().body(u);
+    }
+}
