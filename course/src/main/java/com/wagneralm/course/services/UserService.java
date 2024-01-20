97dea53ad1ab1465746cbbaa17c9bb6b53ea6407
Wagner de Almeida <wagneer.alm@gmail.com>
2024-01-20 00:24:33 -0300

    User update

--- course/src/main/java/com/wagneralm/course/services/UserService.java
+++ course/src/main/java/com/wagneralm/course/services/UserService.java
@@ -30,4 +30,16 @@ public class UserService {
     public void delete(Long id) {
         repository.deleteById(id);
     }
+
+    public User update(Long id, User user) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, user);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setPhone(user.getPhone());
+        entity.setEmail(user.getEmail());
+    }
 }
