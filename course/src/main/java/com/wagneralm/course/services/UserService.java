571d5d2f182948e2cdfd8397c82f941d1f729eea
Wagner de Almeida <wagneer.alm@gmail.com>
2024-01-19 23:51:33 -0300

    User delete

--- course/src/main/java/com/wagneralm/course/services/UserService.java
+++ course/src/main/java/com/wagneralm/course/services/UserService.java
@@ -24,6 +24,10 @@ public class UserService {
     }
 
     public User insert(User user) {
-       return repository.save(user);
+        return repository.save(user);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
     }
 }
