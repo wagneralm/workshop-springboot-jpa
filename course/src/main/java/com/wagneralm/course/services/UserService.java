fb69ffdd8c9c0596d998f248b53a88c6320396b6
Wagner de Almeida <wagneer.alm@gmail.com>
2024-01-24 14:21:54 -0300

    Exception handling - update

--- course/src/main/java/com/wagneralm/course/services/UserService.java
+++ course/src/main/java/com/wagneralm/course/services/UserService.java
@@ -2,8 +2,12 @@ package com.wagneralm.course.services;
 
 import com.wagneralm.course.entities.User;
 import com.wagneralm.course.repositories.UserRepository;
+import com.wagneralm.course.services.exceptions.DatabaseException;
 import com.wagneralm.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,13 +33,24 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try {
+
+            User entity = repository.getReferenceById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
